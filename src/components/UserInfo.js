58f9a7fe8e93c62bdd654911681d4b5e12d26a13
Leksey <mail@leksey.com>
2022-07-18 23:24:37 +0300

    refactoring: UserInfo class + debug

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,16 +1,64 @@
 export default class UserInfo {
-    constructor({ name, about }){
-        this._name = name;
-        this._about = about;
+    constructor({ nameSelector, aboutSelector, getProfileData, setUserInfo, editAvatar}){
+        this._nameSelector = nameSelector;
+        this._aboutSelector = aboutSelector;
+        this._getProfileData = getProfileData;
+        this._setUserInfo = setUserInfo;
+        this._editAvatar = editAvatar;
+    }
+
+    _setProfileData (data) {
+      const profileData = {};
+      profileData.name = data.name;
+      profileData.about = data.about;
+      profileData.avatar = data.avatar;
+      profileData.id = data._id;
+      return profileData;
     }
 
     getUserInfo() {
-        
+        this._getProfileData
+        .then((data) => {
+          return this._setProfileData(data);
+        })
+        .catch((err) => {
+          console.log(err);
+          return err;
+        });
 
-        return ;
     }
 
-    setUserInfo() {
+    setUserInfo(name, about) {
+      this._getProfileData(name, about)
+      .then((data) => {
+        console.log(`_setUserInfo: ${this._setProfileData(data)}`); //TODO: remove before release
+        return this._setProfileData(data);
+        //renderProfile(data); //TODO: Section/render method here
+        //closePopup(popupProfile);
+        //disableButton(profileFormButton, validationElements.inactiveButtonClass);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(function () {
+        changeSubmitButtonText(profileFormButton, 'Сохранить');
+      });
+    }
 
+    editAvatar(picUrl) {
+      this._editAvatar(picUrl)
+      .then((data) => {
+        console.log(`_setUserInfo: ${this._setProfileData(data)}`); //TODO: remove before release
+        return this._setProfileData(data);
+        // renderProfile(data);
+        // closePopup(popupProfilePic);
+        // disableButton(profilePictureFormButton, validationElements.inactiveButtonClass);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(function () {
+        changeSubmitButtonText(profilePictureFormButton, 'Сохранить');
+      });
     }
 }
